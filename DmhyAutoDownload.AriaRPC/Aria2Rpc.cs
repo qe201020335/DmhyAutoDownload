e9e3a5c38a980b276e97589c2c7d8c7ee77470f6
qe201020335 <qe201020335@sina.com>
2024-06-03 22:15:08 -0400

    Aria 2 RPC auto re-connect

--- DmhyAutoDownload.AriaRPC/Aria2Rpc.cs
+++ DmhyAutoDownload.AriaRPC/Aria2Rpc.cs
@@ -27,9 +27,21 @@ public class Aria2Rpc: IDisposable
     
     public void Dispose()
     {
+        DisposeInternal();
+        GC.SuppressFinalize(this);
+    }
+
+    private void DisposeInternal()
+    {
+        Console.WriteLine("Disposing Aria2 RPC...");
         try
         {
-            _rpc?.Dispose();
+            _rpc!.Disconnected -= OnRpcDisconnected;
+            _rpc.Dispose();
+        }
+        catch (NullReferenceException)
+        {
+            // ignored
         }
         catch (Exception e)
         {
@@ -43,12 +55,10 @@ public class Aria2Rpc: IDisposable
         }
     }
 
-    private async Task InitAsync()
+    private async Task InitAsync(CancellationToken cancellationToken = default)
     {
         Console.WriteLine("Initializing Aria2 RPC...");
         Console.WriteLine("Connecting to web socket...");
-        var cancellationTokenSource = new CancellationTokenSource();
-        var cancellationToken = cancellationTokenSource.Token;
         var socket = new ClientWebSocket();
         
         await socket.ConnectAsync(new Uri(_address), cancellationToken);
@@ -67,9 +77,57 @@ public class Aria2Rpc: IDisposable
             EventNameTransform = AriaMethodNameTransforms.ScopedMethod("system")
         });
         rpc.StartListening();
+        
+        rpc.Disconnected += OnRpcDisconnected;
+        
         _rpc = rpc;
         var version = (await _server.GetVersionAsync($"token:{_token}")).version;
         Console.WriteLine($"RPC initialized. Aric2 version: {version}");
+    }
+    
+    private CancellationTokenSource _resetCts = new();
+    
+    private void OnRpcDisconnected(object? sender, JsonRpcDisconnectedEventArgs e)
+    {
+        if (sender != _rpc)
+        {
+            return;
+        }
+        
+        Console.WriteLine("Aria2 RPC got disconnected, resetting in 5 seconds...");
+        _ = Task.Run(async () =>
+        {
+            await _resetCts.CancelAsync();
+            var source = new CancellationTokenSource();
+            var token = source.Token;
+            _resetCts = source;
+            var connected = false;
+            while (!token.IsCancellationRequested && !connected)
+            {
+                try
+                {
+                    await Task.Delay(5000, token);
+                    await ResetAsync(token);
+                    connected = true;
+                }
+                catch (OperationCanceledException exception)
+                {
+                    break;
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"Failed to reset Aria2 RPC: {exception}");
+                    Console.WriteLine("Retrying in 5 seconds...");
+                }
+            }
+        });
+    }
+
+    public async Task ResetAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Aria2 RPC Resetting...");
+        DisposeInternal();
+        await InitAsync(cancellationToken);
     }
     
     public static async Task<Aria2Rpc> CreateAsync(string address, string token)
